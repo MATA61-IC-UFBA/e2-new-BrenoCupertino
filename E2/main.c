314b8092b3e5403c59db30dd668ae619f1381f17
BrenoCupertino <brenonscupertino@gmail.com>
2024-08-12 09:46:23 -0300

    E2 finalizado

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,24 @@
 /* main.c */
 
+#include <stdio.h>
+#include "token.h"
+
+extern int yylex();
+extern char *yytext;
+
+int main()
+{
+    token_t token = -1;
+    while(token != EOL) {
+        token=yylex();
+        if(token == ERROR) {
+            printf("Error: %d\n", token);
+        } else {
+            if (token == NUMBER) {
+                printf("constante decimal: %s\n", yytext);
+            } else {
+                printf("código do token: %d\n", token);
+            }
+        }
+    }
+}
